2f405a2b24f2908dabd955049e4caa56baf2cbac
li changsheng <lichangsheng@lcsMacBook-Air.lan>
2017-09-28 09:04:25 +0800

    new file temp.py

---
+++ temp.py
@@ -0,0 +1,18 @@
+#现在有一个包含 N 个元素的元组或者是序列，怎样将它里面的值解压后同时赋值给 N 个变量？
+
+# var = (3,4,5,2,3,4,5,6)
+# var1 = 3,
+# var2 = 4,
+#......
+
+var = (3,4,5,2,3,4,5,6)
+result = []
+for i in range(0,len(var)):
+	result.append(0) 
+
+for i in range(0,len(var)):
+	result[i] = var[i]
+	print(result[i])
+
+a,b,c,d,e,f,g,h= result
+print(a,b,c)
